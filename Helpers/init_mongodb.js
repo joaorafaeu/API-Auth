82d45fe54cdb7225a7bab8c190dd94507e64aad2
João Rafael Knevitz Vieira <joao-vieira@IPESAUDE.INTRA.RS.GOV.BR>
2023-11-06 09:06:15 -0300

    third commit

--- Helpers/init_mongodb.js
+++ Helpers/init_mongodb.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URI, {
     dbName: process.env.DB_NAME,
 }).then((dbConnection) => {
-    console.log('mongodb connected.')
+    console.log('mongodb connected')
 }).catch((err) => {
     console.log(err.message)
 })
